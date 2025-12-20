a519ddb129a69f2ced1bf5ad53633a7ad92a043a
SaiManjith07 <paripellisaimanjith@gmail.com>
2025-12-20 16:35:02 +0530

    Fix: Add eslint-disable comment for useEffect dependency in ReportLost

--- Frontend/src/pages/user/pets/ReportLost.tsx
+++ Frontend/src/pages/user/pets/ReportLost.tsx
@@ -69,6 +69,7 @@ export default function ReportLost() {
     if (debouncedSpecies || debouncedColor || debouncedLocation) {
       searchMatches();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSpecies, debouncedColor, debouncedLocation]);
 
   const searchMatches = async () => {
