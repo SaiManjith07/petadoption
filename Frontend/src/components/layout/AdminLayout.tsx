a8fd8262ebe5214eb65fe9c0668c4ab24f4c0771
SaiManjith07 <paripellisaimanjith@gmail.com>
2025-12-22 12:45:34 +0530

    Fix All Pets page content being covered by navbar - move pt-16 to main element

--- Frontend/src/components/layout/AdminLayout.tsx
+++ Frontend/src/components/layout/AdminLayout.tsx
@@ -32,9 +32,9 @@ export function AdminLayout({ children, onRefresh, isRefreshing = false }: Admin
       </div>
 
       {/* Main Content */}
-      <div className="flex flex-col min-w-0 lg:ml-64 pt-16">
+      <div className="flex flex-col min-w-0 lg:ml-64">
         {/* Main Content Area - Scrollable */}
-        <main className="flex-1 overflow-y-auto bg-white">
+        <main className="flex-1 overflow-y-auto bg-white pt-16">
           {children}
         </main>
       </div>
